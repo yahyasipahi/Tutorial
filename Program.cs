07f490462c6c05396822fc6025b4b7343d3d6c5c
yahyasipahi <yahyasipahi@hotmail.com>
2016-11-16 20:08:01 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           Console.WriteLine("Tutorial projesi!");
+            Console.Read();
+        }
+    }
+}
